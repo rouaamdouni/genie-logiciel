5e772e660c576c48d4d2aac6f7227eefca43fc98
Roua Amdouni <74329229+rouaamdouni@users.noreply.github.com>
2025-04-12 20:14:46 +0100

    Revert "added strategy-behavior-pattern"

--- strategy-behavior-pattern/before/complete_multiple_choice_question/complete-multiple-choice-question-args.ts
+++
@@ -1,8 +0,0 @@
-import { Type } from "class-transformer"
-import { IsNotEmpty } from "class-validator"
-
-export class CompleteMultipleChoiceQuestionArgs {
-  @IsNotEmpty({ message: "student response is required" })
-  @Type(() => Array<number>)
-  studentResponse: Array<number>
-}
--- strategy-behavior-pattern/before/complete_unique_choice_question/complete-unique-choice-question-args.ts
+++
@@ -1,7 +0,0 @@
-import { IsInt, IsNotEmpty } from "class-validator"
-
-export class CompleteUniqueChoiceQuestionArgs {
-  @IsNotEmpty({ message: "student response is required" })
-  @IsInt()
-  studentResponse: number
-}
